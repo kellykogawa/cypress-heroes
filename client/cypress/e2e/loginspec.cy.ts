dd506dc39508350040779bed26731438a7133026
kellykogawa <kelly19yk@gmail.com>
2024-08-19 11:08:18 -0300

    Login and create new hero specs created

--- client/cypress/e2e/loginspec.cy.ts
+++ client/cypress/e2e/loginspec.cy.ts
@@ -7,3 +7,29 @@ describe('Tentar fazer login com credenciais válidas', () => {
       cy.get('.text-white').click()
     });
   });
+
+
+  describe('Tentar fazer login com email inválido', () => {
+    it('Deve aparecer alerta que o email é inválido', () => {
+      cy.visit('/heroes')
+      cy.get('li > .undefined').click()
+      cy.get('[name="email"]').type("test")
+      cy.get('[name="password"]').type("test")
+      cy.get('.text-white').click()
+      cy.get('.text-red-500')
+        cy.get('body').should('contain', 'Email is not valid')
+    });
+  });
+
+
+  describe('Tentar fazer login com credenciais inválidas', () => {
+    it.only('Deve aparecer alerta que as credenciais são inválidas', () => {
+      cy.visit('/heroes')
+      cy.get('li > .undefined').click()
+      cy.get('[name="email"]').type("test@gmail.com")
+      cy.get('[name="password"]').type("test")
+      cy.get('.text-white').click()
+      cy.get('.text-red-500')
+        cy.get('body').should('contain', 'Invalid email or password')
+    });
+  });
